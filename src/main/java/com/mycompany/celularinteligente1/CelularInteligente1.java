e41ba1968f8843ca8d15ad245c43b147d62c9f27
john <jhonalexander92@hotmail.com>
2023-05-31 20:55:27 -0500

    Carga Inicial

---
+++ src/main/java/com/mycompany/celularinteligente1/CelularInteligente1.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.celularinteligente1;
+
+/**
+ *
+ * @author JHON SANCHEZ
+ */
+public class CelularInteligente1{
+public float tamaño;
+    public String marca;
+    public String color;
+    public int ram;
+    public float procesador;   
+
+public CelularInteligente1(float tamaño, String marca, String color, int ram, float procesador){
+  this.tamaño = tamaño;
+  this.marca = marca;
+  this.color = color;
+  this.ram = ram;
+  this.procesador = procesador;
+  
+  
+}  
+    public static void main(String[] args) {
+    CelularInteligente1 celular1 = new CelularInteligente1(16.5f, "Redmin", "Azul", 4, 2.5f);  
+  System.out.println("Tamaño del celular:" + celular1.tamaño + "\n Marca del celular: " + celular1.marca + "\n Color del celular: " + celular1.color + "\n Ram: " + celular1.ram +"GB"+ "\n Procesador: " + celular1.procesador + "Gz");
+        System.out.println("");
+    }
+  
+}
